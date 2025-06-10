4ba5deeaad79708217ba0ec88d5f354cf8e7bf5a
Alex Levy <ajlevy246@gmail.com>
2025-06-10 14:55:39 -0400

    Added exponential and natural log functions

---
+++ levycas/expressions/exp.py
@@ -0,0 +1,23 @@
+from .expression import Expression
+
+class Exp(Expression):
+    def __init__(self, *args):
+        assert len(args) == 1, "Exp takes only one argument"
+        self.arg = args[0]
+
+    def __repr__(self):
+        return f"exp({self.arg})"
+
+    def operands(self):
+        return [self.arg]
+
+class Ln(Expression):
+    def __init__(self, *args):
+        assert len(args) == 1, "Exp takes only one argument"
+        self.arg = args[0]
+
+    def __repr__(self):
+        return f"ln({self.arg})"
+
+    def operands(self):
+        return [self.arg]
