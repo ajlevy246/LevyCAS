bef7a2f5f344daa04a0c4edaf76c3f80acc8ae44
ajlevy246 <ajlevy246@gmail.com>
2025-12-19 16:54:11 -0500

    line endings

--- src/levycas/__init__.py
+++ src/levycas/__init__.py
@@ -1,3 +1,11 @@
+"""LevyCAS is an implementation of a Computer Algebra System written
+in pure Python. It requires only the built-in regex (re) and math modules.
+
+Check out https://alexlevy.me for a LevyCAS demo!
+
+- Alex Levy (2025)
+"""
+
 from .parser import (
     Token, tokenize,
     TOKEN_SPEC, TOKEN_CLASS, TOKEN_BP, parse, expand, pratt
@@ -23,20 +31,31 @@ from .operations import (
 )
 
 __all__ = [
+    # Lexing Module
     'Token', 'tokenize',
     'TOKEN_SPEC', 'TOKEN_CLASS', 'TOKEN_BP', 'parse', 'expand', 'pratt',
+
+    # Core Expressions Module
     'Constant', 'Integer', 'Rational', 'convert_primitive',
     'UNDEFINED', 'CAS_ENV', 'Expression', 'Sum', 'Product', 'Div', 'Power', 'Factorial', 'Special',
     'trig_substitute', 'Trig', 'Sin', 'Cos', 'Tan', 'Csc', 'Sec', 'Cot', 'Arctan', 'Arccos', 'Arcsin',
     'Variable', 'Function', 'Elementary',
     'Exp', 'Ln',
+
+    # Algebraic Manipulation Module
     'contains', 'copy', 'construct', 'map_op', 'substitute', 'symbols', 'get_symbols',
     'trig_simplify', 'trig_substitute', 'trig_expand', 'trig_contract',
     'is_monomial', 'is_polynomial', 'variables', 'coefficient', 'degree', 'leading_coefficient', 'lex_lt', 'leading_monomial',
     'monomial_divide', 'polynomial_divide', 'polynomial_pseudo_divide', 'polynomial_content', 'polynomial_gcd', 'polynomial_divide_recursive',
     'univariate_partial_fractions',
     'algebraic_expand', 'algebraic_expand_main', 'rationalize', 'linear_form', 'quadratic_form',
+
+    # Symbolic Calculus Module
     'Deriv', 'derivative', 'integrate', 
+
+    # Simplification (Core) Module
     'simplify', 'sym_eval', 'simplify_power', 'simplify_product', 'simplify_sum', 'simplify_div', 'simplify_factorial',
+
+    # Numerical Methods
     'gcd', 'factor_integer', 'is_prime', 'radical', 'rational_simplify'
 ]
