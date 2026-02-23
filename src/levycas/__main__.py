f2672fdd8a7787494c49e2de1603419cb800b821
ajlevy246 <ajlevy246@gmail.com>
2026-02-23 13:58:35 -0500

    Added dependency check and graceful error handling.

--- src/levycas/__main__.py
+++ src/levycas/__main__.py
@@ -1,4 +1,9 @@
 from .cli.main import main
 
 if __name__ == "__main__":
+    try:
+        import textual
+    except ImportError as Exception:
+        raise ImportError("The LevyCAS TUI requires the 'Textual' package. Please see installation instructions in the README.")
+        
     main()
