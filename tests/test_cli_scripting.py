b3c2b093cb920aa4f45f6fdfdb936b06d060a90b
Alex Levy <ajlevy246@gmail.com>
2026-07-02 22:40:45 -0400

    Lexing tests.

--- tests/test_cli_scripting.py
+++ tests/test_cli_scripting.py
@@ -1,9 +1,70 @@
 import pytest
-from levycas.cli.main import LevyCasApp
 
-class TestScripting:
+from levycas.cli.main import LevyCasApp
+from levycas.cli.scripting.scripting import lex_script, run_script, ScriptToken as Token, TokenType as tk
+from levycas.cli.scripting.errors import ExecutionError, ReferenceError
+
+class TestScriptingScreen:
     async def test_screen_load(self):
         cas = LevyCasApp() 
         async with cas.run_test() as pilot:
             await pilot.click()
             assert True
+
+class TestScripting:
+    def test_lexer(self):
+        def tok_eq(expected, actual):
+            val = expected.type == actual.type and expected.literal == actual.literal
+            if not val:
+                print(expected, actual)
+            return val
+        
+        stmt = "f(x) = 4x + 3.5;"
+        lexed = lex_script(stmt)
+        tokens = [
+            Token("f", tk.SYMBOL),  Token("(", tk.LPAREN),
+            Token("x", tk.SYMBOL),  Token(")", tk.RPAREN),
+            Token("=", tk.EQUALS),  Token("4", tk.INTEGER),
+            Token("x", tk.SYMBOL),  Token("+", tk.OPERATION),
+            Token("3.5", tk.FLOAT), Token(";", tk.SEMICOLON),
+        ]
+        assert all(tok_eq(expected, actual) for expected, actual in zip(tokens, lexed, strict=True))
+
+        stmt = "4.55sinxsixn\derivate(for : cos)"
+        lexed = lex_script(stmt)
+        tokens = [
+            Token("4.55", tk.FLOAT), Token("sin", tk.OPERATION),
+            Token("x", tk.SYMBOL), Token("s", tk.SYMBOL),
+            Token("i", tk.SYMBOL), Token("x", tk.SYMBOL),
+            Token("n", tk.SYMBOL), Token("\derivate", tk.COMMAND),
+            Token("(", tk.LPAREN), Token("for", tk.FOR),
+            Token(":", tk.COLON),  Token("cos", tk.OPERATION),
+            Token(")", tk.RPAREN),
+        ]
+        assert all(tok_eq(expected, actual) for expected, actual in zip(tokens, lexed, strict=True))
+
+        #TODO: Update the below behavior if float regex changes
+        # Hard coded as a reminder if it breaks
+        stmt = "print (44.55)"
+        lexed = lex_script(stmt)
+        tokens = [
+            Token("print", tk.PRINT), Token("(", tk.LPAREN),
+            Token("4", tk.INTEGER), Token("4.55", tk.FLOAT),
+            Token(")", tk.RPAREN),
+        ]
+        assert all(tok_eq(expected, actual) for expected, actual in zip(tokens, lexed, strict=True))
+
+    def test_simple_statements(self):
+        ...
+
+    def test_for_loops(self):
+        ...
+
+    def test_print(self):
+        ...
+
+    def test_definitions(self):
+        ...
+
+    def test_substitutions(self):
+        ...
