8b095548ad7ece5676c5c6a708ed90ac5fde7b1b
ajlevy246 <ajlevy246@gmail.com>
2026-07-06 20:05:25 -0700

    test for exp instantiation

--- tests/test_exp.py
+++ tests/test_exp.py
@@ -5,8 +5,8 @@ from levycas.expressions.exp import Ln, Exp
 from levycas.expressions.expression import UNDEFINED
 from levycas.operations.expression_ops import symbols
 
-def test_exp_asimplify():
-    """Tests for the automatic simplification when instantiating."""
+def test_ln_asimplify():
+    """Tests for the automatic simplification of Ln when instantiating."""
     x, y = symbols("x y")
     with pytest.raises(ValueError):
         Ln(0)
@@ -15,3 +15,8 @@ def test_exp_asimplify():
     assert Ln(x**2 * y**3) == 2*Ln(x) + 3*Ln(y)
     assert Ln(1/x**2) == -2*Ln(x)
     assert Ln(x*y) == Ln(x) + Ln(y)
+
+def test_exp_asimplify():
+    """Tests for the automatic simplification of Exp when instantiating."""
+    x, y = symbols("x y")
+    assert Exp(0) == 1
