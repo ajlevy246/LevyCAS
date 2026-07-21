aaf985f924198baf1ae8c06bf6fa46ba590ce040
Alex Levy <ajlevy246@gmail.com>
2026-07-21 10:25:25 -0400

    Tests for exponential/logarithm simplification routines

--- tests/test_exp.py
+++ tests/test_exp.py
@@ -4,10 +4,15 @@ import pytest
 from levycas.expressions.exp import Ln, Exp
 from levycas.expressions.expression import UNDEFINED
 from levycas.operations.expression_ops import symbols
+from levycas.operations.exponential_ops import (
+    exp_contract, exp_expand, exp_simplify,
+    log_expand,
+)
+
+x, y, w, z = symbols("x y w z")
 
 def test_ln_asimplify():
     """Tests for the automatic simplification of Ln when instantiating."""
-    x, y = symbols("x y")
     with pytest.raises(ValueError):
         Ln(0)
     assert Ln(1) == 0
@@ -18,5 +23,69 @@ def test_ln_asimplify():
 
 def test_exp_asimplify():
     """Tests for the automatic simplification of Exp when instantiating."""
-    x, y = symbols("x y")
     assert Exp(0) == 1
+
+def test_exp_expand():
+    """Tests for the exp_expand() operator."""
+    assert (
+        exp_expand(Exp(2*w*x + 3*y*z))
+        == exp_expand(Exp(2*w*x)*Exp(3*y*z))
+        == Exp(w*x)**2 * Exp(y*z)**3
+    )
+    assert (
+        exp_expand(Exp(2*(x+y)))
+        == exp_expand(Exp(x+y)**2)
+        == Exp(x)**2 * Exp(y)**2
+    )
+    assert (
+        exp_expand(Exp((x+y)*(x-y)))
+        == Exp(x**2) / Exp(y**2)
+    )
+    assert (
+        exp_expand(1 / (Exp(2*x)-Exp(x)**2))
+        is UNDEFINED
+    )
+
+def test_exp_contract():
+    """Tests for the exp_contract() operator."""
+    assert (
+        exp_contract(Exp(x)*Exp(y))
+        == Exp(x+y)
+    )
+    assert (
+        exp_contract(Exp(x)**y)
+        == Exp(x*y)
+    )
+    assert (
+        exp_contract(Exp(x)*(Exp(x)+Exp(y)))
+        == exp_contract(Exp(x)**2 + Exp(x)*Exp(y))
+        == Exp(2*x) + Exp(x+y)
+    )
+    assert (
+        exp_contract(Exp(Exp(x))**Exp(y))
+        == exp_contract(Exp(Exp(x)*Exp(y)))
+        == Exp(Exp(x+y))
+    )
+
+def test_exp_simplify():
+    """Tests for the exp_simplify() operator."""
+    assert (
+        exp_simplify(1/(Exp(x)*(Exp(y)+Exp(-x))) - (Exp(x+y)-1)/(Exp(x+y)**2 - 1))
+        == 0
+    )
+
+def test_log_expand():
+    """Tests for the log_expand operator."""
+    assert (
+        log_expand(Ln(x*y))
+        == Ln(x) + Ln(y)
+    )
+    assert (
+        log_expand(Ln(x**y))
+        == y*Ln(x)
+    )
+    a, b = symbols("a b")
+    assert (
+        log_expand(Ln((w*x)**a)) + Ln(y**b*z)
+        == a*(Ln(w) + Ln(x)) + b*Ln(y) + Ln(z)
+    )
