60f3141071ef102200e263871711354a27294371
Alex Levy <ajlevy246@gmail.com>
2026-07-23 12:39:29 -0400

    Added visual regression test for scripting screen + snapshot

--- tests/test_tui_scripting.py
+++ tests/test_tui_scripting.py
@@ -1,4 +1,7 @@
 import pytest
+from pytest_textual_snapshot import snap_compare
+from textual.widgets import TextArea
+from textual.pilot import Pilot
 
 from levycas.cli.__main__ import LevyCasApp
 
@@ -8,12 +11,16 @@ class TestScriptingScreen:
         async with cas.run_test() as pilot:
             await pilot.click("#scripting")
 
+    async def run_example(self, pilot):
+        await pilot.resize_terminal(125, 40)
+        await pilot.click("#scripting")
+        await pilot.click("#load-example")
+        await pilot.click("#run-script")
+
     async def test_script_example(self):
         cas = LevyCasApp()
         async with cas.run_test() as pilot:
-            await pilot.click("#scripting")
-            await pilot.click("#load-example")
-            await pilot.click("#run-script")
+            await self.run_example(pilot)
             output_log = cas.screen.script_output
             assert output_log.lines == [
                 "-yCos(y) + yCos(x) + Sin(y)",
@@ -22,3 +29,7 @@ class TestScriptingScreen:
                 "Cos(2)",
                 "Cos(3)",
             ]
+
+    def test_script_snapshot(self, snap_compare):
+        cas = LevyCasApp()
+        assert snap_compare(cas, run_before=self.run_example)
