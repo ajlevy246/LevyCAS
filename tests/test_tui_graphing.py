79fdf2e6434b67a20d563ea265cc635df3cea7ab
Alex Levy <ajlevy246@gmail.com>
2026-08-11 10:39:09 -0400

    tests graph actions and panning

--- tests/test_tui_graphing.py
+++ tests/test_tui_graphing.py
@@ -1,7 +1,7 @@
 import pytest
 
 from textual.pilot import Pilot
-from textual.events import MouseScrollUp
+from textual.events import MouseScrollUp, MouseScrollDown
 from textual.containers import Center
 from pytest_textual_snapshot import snap_compare
 
@@ -13,32 +13,33 @@ class TestGraphing:
         async with cas.run_test() as pilot:
             await pilot.click("#graphing")
 
-    def test_plots_simple_Uses(self, snap_compare):
+    def test_plots_simple_uses(self, snap_compare):
         cas = LevyCasApp()
         async def graph_exprs(pilot: Pilot):
-            await pilot.resize_terminal(125, 40)
             await pilot.click("#graphing")
             await pilot.press("tab")
             await pilot._post_mouse_events([MouseScrollUp], times=5, widget="#plot")
 
-            exprs = ["x^3", "sin(x)", "ln(x)x^(1/2)", "cos(x)^2+sin(x)^2-0.5"]
+            exprs = ["x^3", "sin(sin(sin(sin(x))))", "ln(x)x^(1/2)", "cos(x)^2+sin(x)^2-0.5"]
             for expr in exprs:
-                await pilot.press(*expr)
-                await pilot.press("enter")
+                await pilot.press(*expr, "enter")
 
-        assert snap_compare(cas, run_before=graph_exprs)
+
+        assert snap_compare(
+            cas,
+            terminal_size=(125, 40),
+            run_before=graph_exprs,
+        )
 
     def test_plots_colors_and_delete(self, snap_compare):
         cas = LevyCasApp()
         async def graph_color_and_delete(pilot: Pilot):
-            await pilot.resize_terminal(125, 40)
             await pilot.click("#graphing")
             await pilot.press("tab")
 
             exprs = ["tan(x-5)-2", "exp(cos(x))", "ln(x^2)", "(x+1)/(x-1)"]
             for expr in exprs:
-                await pilot.press(*expr)
-                await pilot.press("enter")
+                await pilot.press(*expr, "enter")
 
             for i in range(len(exprs)-1):
                 await pilot.click(f"#expression-delete-{i}")
@@ -51,11 +52,69 @@ class TestGraphing:
             await pilot.click("#expression-color-0")
             await pilot.click("#expression-color-0")
 
-        assert snap_compare(cas, run_before=graph_color_and_delete)
+        assert snap_compare(
+            cas, 
+            terminal_size=(125, 40),
+            run_before=graph_color_and_delete,
+        )
 
-    def test_plots_actions_and_reset(self, snap_compare):
+    def test_plot_actions(self, snap_compare):
         cas = LevyCasApp()
         async def graph_actions(pilot: Pilot):
-            ...
+            await pilot.click("#graphing")
+            await pilot.press("tab")
 
-        assert snap_compare(cas, run_before=graph_actions)
+            exprs = ["x", "x^2+2", "x^3 - 3", "x^4 + 4"]
+            for expr in exprs:
+                await pilot.press(*expr, "enter")
+
+            await pilot.click("#plot")
+
+            # try disabling gridlines
+            await pilot.press("g")
+
+            # and disabling legend
+            await pilot.press("l")
+
+            # move plot around
+            for _ in range(15):
+                await pilot.press("right", "up")
+            await pilot._post_mouse_events([MouseScrollUp], times=5, widget="#plot")
+
+        assert snap_compare(
+            cas,
+            terminal_size=(125, 40),
+            run_before=graph_actions
+        )
+
+    def test_plot_reset_action(self, snap_compare):
+        cas = LevyCasApp()
+        async def graph_actions(pilot: Pilot):
+            await pilot.click("#graphing")
+            await pilot.press("tab")
+
+            exprs = ["x", "x^2+2", "x^3 - 3", "x^4 + 4"]
+            for expr in exprs:
+                await pilot.press(*expr, "enter")
+
+            await pilot.click("#plot")
+
+            # try disabling gridlines
+            await pilot.press("g")
+
+            # and disabling legend
+            await pilot.press("l")
+
+            # move plot around
+            for _ in range(15):
+                await pilot.press("right", "up")
+            await pilot._post_mouse_events([MouseScrollUp], times=5, widget="#plot")
+
+            # reset plot; should see gridlines and legend back at default axes.
+            await pilot.press("r")
+
+        assert snap_compare(
+            cas,
+            terminal_size=(125, 40),
+            run_before=graph_actions
+        )
